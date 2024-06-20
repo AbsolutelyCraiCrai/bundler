8764cb341b772bac312eb5e924059782feb8c555
Absolutely CraiCrai <jempcorp@ymail.com>
2024-06-20 11:33:00 +0100

    Initial commit

---
+++ Bundler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Bundler
+{
+    internal class Bundler
+    {
+        public static void Main(String[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Invalid arguments.");
+                return;
+            }
+
+            string dir = args[0];
+
+            string[] files = Directory.EnumerateFiles(dir, "*.js", SearchOption.AllDirectories).ToArray();
+            string bundled = "";
+            foreach (string file in files)
+            {
+                bundled += File.ReadAllText(file);
+            }
+
+            bundled = Regex.Replace(bundled, "import .+? from \"\\..+?\";", "", RegexOptions.IgnoreCase);
+            bundled = Regex.Replace(bundled, "export .+?;", "", RegexOptions.IgnoreCase);
+
+            File.WriteAllText("input.js", bundled);
+
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "esbuild.exe";
+            p.StartInfo.Arguments = "input.js --outdir=out --minify --sourcemap";
+            p.StartInfo.UseShellExecute = false;
+            p.Start();
+            p.WaitForExit();
+        }
+    }
+}
